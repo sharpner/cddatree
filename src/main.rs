2b8d47e820b71358c280bece4e70156aec2dfe70
n.wagensonner <n.wagensonner@manyminds.de>
2016-10-05 21:00:41 +0200

    Print all file names
    wow that syntax....oO

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::path::Path;
+use std::io;
+
+static RECIPES_FOLDER: &'static str="vendor/cdda/recipes";
+
+fn visit_dirs(dir: &Path, cb: &Fn(&fs::DirEntry)) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in try!(fs::read_dir(dir)) {
+            let entry = try!(entry);
+            let path = entry.path();
+            if path.is_dir() {
+                try!(visit_dirs(&path, cb));
+            } else {
+                cb(&entry);
+            }
+        }
+    }
+    Ok(())
 }
+
+fn entry(cb: &fs::DirEntry) -> () {
+	println!("{:?}", cb.path());
+}
+
+fn main() {
+	let _ = visit_dirs(Path::new(RECIPES_FOLDER), &entry);
+}
+
